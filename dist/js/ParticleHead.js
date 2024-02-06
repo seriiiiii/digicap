eb9a69bfd08c486c7091f998a20ed9d0bb32a928
[디지캡] 박세리 <srpark@digicaps.com>
2024-02-06 17:14:13 +0900

    JS수정

--- dist/js/ParticleHead.js
+++ dist/js/ParticleHead.js
@@ -4,116 +4,116 @@ site.document = $(document);
 site.Width = site.window.width();
 site.Height = site.window.height();
 
-var Background = function() {
+var Background = function () {};
 
-};
+Background.headparticle = function () {
+  if (!Modernizr.webgl) {
+    alert('Your browser dosent support WebGL');
+  }
 
-Background.headparticle = function() {   
+  var camera, scene, renderer;
+  var mouseX = 0,
+    mouseY = 0;
+  var p;
 
-   if ( !Modernizr.webgl ) {
-      alert('Your browser dosent support WebGL');
-   }
+  var windowHalfX = site.Width / 2;
+  var windowHalfY = site.Height / 2;
 
-   var camera, scene, renderer;
-   var mouseX = 0, mouseY = 0;
-   var p;
+  Background.camera = new THREE.PerspectiveCamera(
+    35,
+    site.Width / site.Height,
+    1,
+    2000,
+  );
+  Background.camera.position.z = 300;
 
-   var windowHalfX = site.Width / 2;
-   var windowHalfY = site.Height / 2;
+  // scene
+  Background.scene = new THREE.Scene();
 
-   Background.camera = new THREE.PerspectiveCamera( 35, site.Width / site.Height, 1, 2000 );
-   Background.camera.position.z = 300;
+  // texture
+  var manager = new THREE.LoadingManager();
+  manager.onProgress = function (item, loaded, total) {
+    //console.log('webgl, twice??');
+    //console.log( item, loaded, total );
+  };
 
-   // scene
-   Background.scene = new THREE.Scene();
+  // particles
+  var p_geom = new THREE.Geometry();
+  var p_material = new THREE.ParticleBasicMaterial({
+    // color: 0xD6D9E1,
+    color: 0xffffff,
+    size: 1.9,
+  });
 
-   // texture
-   var manager = new THREE.LoadingManager();
-   manager.onProgress = function ( item, loaded, total ) {
-      //console.log('webgl, twice??');
-      //console.log( item, loaded, total );
-   };
+  // model
+  var loader = new THREE.OBJLoader(manager);
+  loader.load(
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/40480/head.obj',
+    function (object) {
+      object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+          // child.material.map = texture;
 
+          var scale = 8;
 
-   // particles
-   var p_geom = new THREE.Geometry();
-   var p_material = new THREE.ParticleBasicMaterial({
-      // color: 0xD6D9E1,
-      color: 0xFFFFFF,
-      size: 1.9
-   });
-
-   // model
-   var loader = new THREE.OBJLoader( manager );
-   loader.load( 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/40480/head.obj', function ( object ) {
-
-      object.traverse( function ( child ) {
-
-         if ( child instanceof THREE.Mesh ) {
-
-            // child.material.map = texture;
-
-            var scale = 8;
-
-            $(child.geometry.vertices).each(function() {
-               p_geom.vertices.push(new THREE.Vector3(this.x * scale, this.y * scale, this.z * scale));
-            })
-         }
+          $(child.geometry.vertices).each(function () {
+            p_geom.vertices.push(
+              new THREE.Vector3(this.x * scale, this.y * scale, this.z * scale),
+            );
+          });
+        }
       });
 
-      Background.scene.add(p)
-   });
+      Background.scene.add(p);
+    },
+  );
 
-   p = new THREE.ParticleSystem(
-      p_geom,
-      p_material
-      );
+  p = new THREE.ParticleSystem(p_geom, p_material);
 
-   Background.renderer = new THREE.WebGLRenderer({ alpha: true });
-   Background.renderer.setSize( site.Width, site.Height );
-   Background.renderer.setClearColor(0x000000, 0);
+  Background.renderer = new THREE.WebGLRenderer({ alpha: true });
+  Background.renderer.setSize(site.Width, site.Height);
+  Background.renderer.setClearColor(0x000000, 0);
 
-   $('.particlehead').append(Background.renderer.domElement);
-   $('.particlehead').on('mousemove', onDocumentMouseMove);
-   site.window.on('resize', onWindowResize);
+  $('.particlehead').append(Background.renderer.domElement);
+  $('body').on('mousemove', onDocumentMouseMove);
+  site.window.on('resize', onWindowResize);
 
-   function onWindowResize() {
-      windowHalfX = site.Width / 2;
-      windowHalfY = site.Height / 2;
-      //console.log(windowHalfX);
+  function onWindowResize() {
+    windowHalfX = site.Width / 2;
+    windowHalfY = site.Height / 2;
+    //console.log(windowHalfX);
 
-      Background.camera.aspect = site.Width / site.Height;
-      Background.camera.updateProjectionMatrix();
+    Background.camera.aspect = site.Width / site.Height;
+    Background.camera.updateProjectionMatrix();
 
-      Background.renderer.setSize( site.Width, site.Height );
-   }
+    Background.renderer.setSize(site.Width, site.Height);
+  }
 
-   function onDocumentMouseMove( event ) {
-      mouseX = ( event.clientX - windowHalfX ) / 2;
-      mouseY = ( event.clientY - windowHalfY ) / 2;
-   }
+  function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowHalfX) / 2;
+    mouseY = (event.clientY - windowHalfY) / 2;
+  }
 
-   Background.animate = function() { 
+  Background.animate = function () {
+    Background.ticker = TweenMax.ticker;
+    Background.ticker.addEventListener('tick', Background.animate);
 
-      Background.ticker = TweenMax.ticker;
-      Background.ticker.addEventListener("tick", Background.animate);
+    render();
+  };
 
-      render();
-   }
+  function render() {
+    Background.camera.position.x +=
+      (-(mouseX * 0.5) - Background.camera.position.x) * 0.05; // 마우스가 바라보는 방향으로 변경
+    Background.camera.position.y +=
+      (mouseY * 0.5 - Background.camera.position.y) * 0.05; // 마우스가 바라보는 방향으로 변경
 
-   function render() {
-      Background.camera.position.x += ( (mouseX * .5) - Background.camera.position.x ) * .05;
-      Background.camera.position.y += ( -(mouseY * .5) - Background.camera.position.y ) * .05;
+    Background.camera.lookAt(Background.scene.position);
+    Background.renderer.render(Background.scene, Background.camera);
+  }
 
-      Background.camera.lookAt( Background.scene.position );
+  render();
 
-      Background.renderer.render( Background.scene, Background.camera );
-   }
-
-   render();
-
-   Background.animate();
+  Background.animate();
 };
-
 
 Background.headparticle();
